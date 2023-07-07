41eb680fc93a5e697a6d07fdf42b4f9e3ce0c112
dimitrmo <dmouratidis153@gmail.com>
2023-07-07 22:02:52 +0300

    Use single lock

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use actix_web::body::BoxBody;
-use actix_web::http::StatusCode;
-use actix_web::{get, guard, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use cygaz_lib::{fetch_prices, PetroleumStation, PetroleumType};
 use log::{debug, info, warn};
 use reqwest::header::HeaderMap;
 use reqwest::{Error, Response};
 use serde::{Deserialize, Serialize};
-use std::sync::{Arc, LockResult, RwLock, RwLockReadGuard, RwLockWriteGuard};
+use std::sync::{Arc, RwLock};
 use std::thread;
 use std::time::{SystemTime, UNIX_EPOCH};
 use tokio_cron_scheduler::{Job, JobScheduler};
@@ -16,17 +15,7 @@ use uuid::Uuid;
 struct PriceList {
     updated_at: u128,
     petroleum_type: PetroleumType,
-    stations: Box<Vec<PetroleumStation>>,
-}
-
-impl PriceList {
-    fn empty(petroleum_type: PetroleumType, updated_at: u128) -> Self {
-        PriceList {
-            petroleum_type,
-            updated_at,
-            stations: Box::new(vec![]),
-        }
-    }
+    stations: Vec<PetroleumStation>,
 }
 
 fn default_port() -> u16 {
@@ -52,11 +41,11 @@ struct Config {
 }
 
 struct AppStateWithPrices {
-    unlead95: RwLock<PriceList>,
-    unlead98: RwLock<PriceList>,
-    diesel_heat: RwLock<PriceList>,
-    diesel_auto: RwLock<PriceList>,
-    kerosene: RwLock<PriceList>,
+    unlead95: PriceList,
+    unlead98: PriceList,
+    diesel_heat: PriceList,
+    diesel_auto: PriceList,
+    kerosene: PriceList,
 }
 
 impl Responder for PriceList {
@@ -69,187 +58,10 @@ impl Responder for PriceList {
     }
 }
 
-fn fetch_petroleum(
-    petroleum_type: PetroleumType,
-    lock: LockResult<RwLockReadGuard<'_, PriceList>>,
-) -> PriceList {
-    let epoch = SystemTime::now().duration_since(UNIX_EPOCH);
-    let epoch_updated_at = epoch.unwrap().as_millis();
-    if lock.is_err() {
-        return PriceList::empty(petroleum_type, epoch_updated_at);
-    }
-
-    let petroleum_list = lock.unwrap();
-    let petr_stations = petroleum_list.stations.clone();
-    let petr_updated_at = petroleum_list.updated_at;
-
-    PriceList {
-        petroleum_type,
-        updated_at: petr_updated_at,
-        stations: petr_stations,
-    }
-}
-
-fn refresh_petroleum(
-    petroleum_type: PetroleumType,
-    lock: LockResult<RwLockWriteGuard<'_, PriceList>>,
+fn refresh_prices(
+    prices: web::Data<Arc<RwLock<AppStateWithPrices>>>
 ) {
-    debug!("refreshing prices for {}", petroleum_type as i32);
-
-    let stations = fetch_prices(petroleum_type).unwrap_or_default();
-    debug!(
-        "found {} station/prices for {}",
-        stations.len(),
-        petroleum_type as i32
-    );
-
-    // fetch timestamp
-    let epoch = SystemTime::now().duration_since(UNIX_EPOCH);
-    let epoch_updated_at = epoch.unwrap().as_millis();
-
-    // update local cache
-    let mut petroleum_list = lock.unwrap();
-    *petroleum_list = PriceList {
-        petroleum_type,
-        updated_at: epoch_updated_at,
-        stations: Box::new(stations),
-    };
-}
-
-#[get("/prices/1")]
-async fn unlead95(data: web::Data<AppStateWithPrices>) -> impl Responder {
-    fetch_petroleum(PetroleumType::Unlead95, data.unlead95.read())
-}
-
-#[get("/prices/2")]
-async fn unlead98(data: web::Data<AppStateWithPrices>) -> impl Responder {
-    fetch_petroleum(PetroleumType::Unlead98, data.unlead98.read())
-}
-
-#[get("/prices/3")]
-async fn diesel_heat(data: web::Data<AppStateWithPrices>) -> impl Responder {
-    fetch_petroleum(PetroleumType::DieselHeat, data.diesel_heat.read())
-}
-
-#[get("/prices/4")]
-async fn diesel_auto(data: web::Data<AppStateWithPrices>) -> impl Responder {
-    fetch_petroleum(PetroleumType::DieselAuto, data.diesel_auto.read())
-}
-
-#[get("/prices/5")]
-async fn kerosene(data: web::Data<AppStateWithPrices>) -> impl Responder {
-    fetch_petroleum(PetroleumType::Kerosene, data.kerosene.read())
-}
-
-#[get("/version")]
-async fn version() -> impl Responder {
-    env!("CARGO_PKG_VERSION")
-}
-
-async fn refresh_unlead95(data: web::Data<AppStateWithPrices>) -> impl Responder {
-    thread::spawn(move || {
-        refresh_petroleum(PetroleumType::Unlead95, data.unlead95.write());
-    });
-    HttpResponse::Ok().status(StatusCode::NO_CONTENT).finish()
-}
-
-async fn refresh_unlead98(data: web::Data<AppStateWithPrices>) -> impl Responder {
-    thread::spawn(move || {
-        refresh_petroleum(PetroleumType::Unlead98, data.unlead98.write());
-    });
-    HttpResponse::Ok().status(StatusCode::NO_CONTENT).finish()
-}
-
-async fn refresh_diesel_heat(data: web::Data<AppStateWithPrices>) -> impl Responder {
-    thread::spawn(move || {
-        refresh_petroleum(PetroleumType::DieselHeat, data.diesel_heat.write());
-    });
-    HttpResponse::Ok().status(StatusCode::NO_CONTENT).finish()
-}
-
-async fn refresh_diesel_auto(data: web::Data<AppStateWithPrices>) -> impl Responder {
-    thread::spawn(move || {
-        refresh_petroleum(PetroleumType::DieselAuto, data.diesel_auto.write());
-    });
-    return HttpResponse::Ok().status(StatusCode::NO_CONTENT).finish();
-}
-
-async fn refresh_kerosene(data: web::Data<AppStateWithPrices>) -> impl Responder {
-    refresh_petroleum(PetroleumType::Kerosene, data.diesel_auto.write());
-    return HttpResponse::Ok().status(StatusCode::NO_CONTENT).finish();
-}
-
-async fn refresh_petroleum_type(
-    config: Arc<Config>,
-    petroleum_type: PetroleumType,
-) -> Result<Response, Error> {
-    let endpoint = format!(
-        "http://{}:{}/prices/{}/refresh",
-        config.host, config.port, petroleum_type as i32
-    );
-
-    info!("calling {}", endpoint);
-
-    let mut headers = HeaderMap::new();
-    headers.insert("X-TOKEN", config.secret.parse().unwrap());
-
-    let client = reqwest::Client::new();
-    client.patch(endpoint).headers(headers).send().await
-}
-
-async fn setup_cron(config: Arc<Config>) -> JobScheduler {
-    debug!("setting up cron");
-
-    let sched = JobScheduler::new().await.unwrap();
-
-    if let Err(e) = sched.add(
-        Job::new_async("0 1,16,31,46 * * * *", move |_uuid, _l| {
-            let config = config.clone();
-            Box::pin(async move {
-                if let Err(e) =
-                    refresh_petroleum_type(config.clone(), PetroleumType::Unlead95).await
-                {
-                    warn!("error refreshing unlead95 {}", e);
-                }
-                if let Err(e) =
-                    refresh_petroleum_type(config.clone(), PetroleumType::Unlead98).await
-                {
-                    warn!("error refreshing unlead98 {}", e);
-                }
-                if let Err(e) =
-                    refresh_petroleum_type(config.clone(), PetroleumType::DieselHeat).await
-                {
-                    warn!("error refreshing diesel heat {}", e);
-                }
-                if let Err(e) =
-                    refresh_petroleum_type(config.clone(), PetroleumType::DieselAuto).await
-                {
-                    warn!("error refreshing diesel auto {}", e);
-                }
-                if let Err(e) =
-                    refresh_petroleum_type(config.clone(), PetroleumType::Kerosene).await
-                {
-                    warn!("error refreshing kerosene {}", e);
-                }
-            })
-        })
-        .unwrap(),
-    ).await {
-        warn!("error scheduling {:?}", e);
-    }
-
-    sched
-}
-
-#[tokio::main]
-async fn main() -> std::io::Result<()> {
-    env_logger::init();
-
-    let config = Arc::new(envy::from_env::<Config>().unwrap());
-    let address = format!("{}:{}", config.host, config.port);
-    let epoch = SystemTime::now().duration_since(UNIX_EPOCH);
-
-    info!("warming up cache");
+    debug!("refreshing prices");
 
     let unlead95_handler = thread::spawn(|| {
         debug!("warming up unlead 95");
@@ -276,54 +88,204 @@ async fn main() -> std::io::Result<()> {
         fetch_prices(PetroleumType::Kerosene).unwrap_or_default()
     });
 
-    let unlead95_stations = unlead95_handler.join().unwrap();
-    let unlead98_stations = unlead98_handler.join().unwrap();
-    let diesel_heat_stations = diesel_heat_handler.join().unwrap();
-    let diesel_auto_stations = diesel_auto_handler.join().unwrap();
-    let kerosene_stations = kerosene_handler.join().unwrap();
+    let unlead95_stations = unlead95_handler.join().unwrap_or_default();
+    let unlead98_stations = unlead98_handler.join().unwrap_or_default();
+    let diesel_heat_stations = diesel_heat_handler.join().unwrap_or_default();
+    let diesel_auto_stations = diesel_auto_handler.join().unwrap_or_default();
+    let kerosene_stations = kerosene_handler.join().unwrap_or_default();
 
-    let updated_at = epoch.unwrap().as_millis();
+    // fetch timestamp
+    let epoch = SystemTime::now().duration_since(UNIX_EPOCH);
+    let epoch_updated_at = epoch.unwrap().as_millis();
 
-    let data = web::Data::new(AppStateWithPrices {
-        unlead95: RwLock::new(PriceList {
-            petroleum_type: PetroleumType::Unlead95,
-            updated_at,
-            stations: Box::new(unlead95_stations),
-        }),
-        unlead98: RwLock::new(PriceList {
-            petroleum_type: PetroleumType::Unlead98,
-            updated_at,
-            stations: Box::new(unlead98_stations),
-        }),
-        diesel_heat: RwLock::new(PriceList {
-            petroleum_type: PetroleumType::DieselHeat,
-            updated_at,
-            stations: Box::new(diesel_heat_stations),
-        }),
-        diesel_auto: RwLock::new(PriceList {
-            petroleum_type: PetroleumType::DieselAuto,
-            updated_at,
-            stations: Box::new(diesel_auto_stations),
-        }),
-        kerosene: RwLock::new(PriceList {
-            petroleum_type: PetroleumType::Kerosene,
-            updated_at,
-            stations: Box::new(kerosene_stations),
-        }),
-    });
+    let mut lock = prices.write().unwrap();
 
-    let scheduler = setup_cron(config.clone());
+    lock.unlead95 = PriceList {
+        petroleum_type: PetroleumType::Unlead95,
+        updated_at: epoch_updated_at,
+        stations: unlead95_stations,
+    };
 
-    if let Err(e) = scheduler.await.start().await {
-        warn!("scheduler error {:?}", e);
+    lock.unlead98 = PriceList {
+        petroleum_type: PetroleumType::Unlead98,
+        updated_at: epoch_updated_at,
+        stations: unlead98_stations,
+    };
+
+    lock.diesel_heat = PriceList {
+        petroleum_type: PetroleumType::DieselHeat,
+        updated_at: epoch_updated_at,
+        stations: diesel_heat_stations,
+    };
+
+    lock.diesel_auto = PriceList {
+        petroleum_type: PetroleumType::DieselAuto,
+        updated_at: epoch_updated_at,
+        stations: diesel_auto_stations,
+    };
+
+    lock.kerosene = PriceList {
+        petroleum_type: PetroleumType::Kerosene,
+        updated_at: epoch_updated_at,
+        stations: kerosene_stations,
+    };
+}
+
+#[get("/prices/1")]
+async fn unlead95(data: web::Data<Arc<RwLock<AppStateWithPrices>>>) -> impl Responder {
+    let state = data.read().unwrap();
+    state.unlead95.clone()
+}
+
+#[get("/prices/2")]
+async fn unlead98(data: web::Data<Arc<RwLock<AppStateWithPrices>>>) -> impl Responder {
+    let state = data.read().unwrap();
+    state.unlead98.clone()
+}
+
+#[get("/prices/3")]
+async fn diesel_heat(data: web::Data<Arc<RwLock<AppStateWithPrices>>>) -> impl Responder {
+    let state = data.read().unwrap();
+    state.diesel_heat.clone()
+}
+
+#[get("/prices/4")]
+async fn diesel_auto(data: web::Data<Arc<RwLock<AppStateWithPrices>>>) -> impl Responder {
+    let state = data.read().unwrap();
+    state.diesel_auto.clone()
+}
+
+#[get("/prices/5")]
+async fn kerosene(data: web::Data<Arc<RwLock<AppStateWithPrices>>>) -> impl Responder {
+    let state = data.read().unwrap();
+    state.kerosene.clone()
+}
+
+#[get("/version")]
+async fn version() -> impl Responder {
+    env!("CARGO_PKG_VERSION")
+}
+
+async fn refresh_petroleum_type(
+    config: Arc<Config>,
+    petroleum_type: PetroleumType,
+) -> Result<Response, Error> {
+    let endpoint = format!(
+        "http://{}:{}/prices/{}/refresh",
+        config.host, config.port, petroleum_type as i32
+    );
+
+    info!("calling {}", endpoint);
+
+    let mut headers = HeaderMap::new();
+    headers.insert("X-TOKEN", config.secret.parse().unwrap());
+
+    let client = reqwest::Client::new();
+    client.patch(endpoint).headers(headers).send().await
+}
+
+async fn setup_cron(config: Arc<Config>, prices: web::Data<Arc<RwLock<AppStateWithPrices>>>) -> JobScheduler {
+    debug!("setting up cron");
+
+    let sched = JobScheduler::new().await.unwrap();
+
+    if let Err(e) = sched.add(
+        Job::new_async("0 1,16,31,46 * * * *", move |_uuid, _l| {
+            let config = config.clone();
+            let prices = prices.clone();
+
+            Box::pin(async move {
+                if let Err(e) =
+                    refresh_petroleum_type(config.clone(), PetroleumType::Unlead95).await
+                {
+                    warn!("error refreshing unlead95 {}", e);
+                }
+                if let Err(e) =
+                    refresh_petroleum_type(config.clone(), PetroleumType::Unlead98).await
+                {
+                    warn!("error refreshing unlead98 {}", e);
+                }
+                if let Err(e) =
+                    refresh_petroleum_type(config.clone(), PetroleumType::DieselHeat).await
+                {
+                    warn!("error refreshing diesel heat {}", e);
+                }
+                if let Err(e) =
+                    refresh_petroleum_type(config.clone(), PetroleumType::DieselAuto).await
+                {
+                    warn!("error refreshing diesel auto {}", e);
+                }
+                if let Err(e) =
+                    refresh_petroleum_type(config.clone(), PetroleumType::Kerosene).await
+                {
+                    warn!("error refreshing kerosene {}", e);
+                }
+
+                refresh_prices(prices);
+
+                info!("scheduler finished successfully");
+            })
+        })
+        .unwrap(),
+    ).await {
+        warn!("error scheduling {:?}", e);
     }
 
-    let konfig = config.clone();
+    sched
+}
 
-    info!("starting http server");
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let raw = envy::from_env::<Config>().unwrap();
+    let config = Arc::new(raw);
+    let address = format!("{}:{}", config.host, config.port);
+
+    let epoch = SystemTime::now().duration_since(UNIX_EPOCH);
+    let updated_at = epoch.unwrap().as_millis();
+
+    info!("warming up initial cache");
+
+    let data = web::Data::new(Arc::new(RwLock::new(AppStateWithPrices {
+        unlead95: PriceList {
+            petroleum_type: PetroleumType::Unlead95,
+            updated_at,
+            stations: vec![],
+        },
+        unlead98: PriceList {
+            petroleum_type: PetroleumType::Unlead98,
+            updated_at,
+            stations: vec![],
+        },
+        diesel_heat: PriceList {
+            petroleum_type: PetroleumType::DieselHeat,
+            updated_at,
+            stations: vec![],
+        },
+        diesel_auto: PriceList {
+            petroleum_type: PetroleumType::DieselAuto,
+            updated_at,
+            stations: vec![],
+        },
+        kerosene: PriceList {
+            petroleum_type: PetroleumType::Kerosene,
+            updated_at,
+            stations: vec![],
+        },
+    })));
+
+    refresh_prices(data.clone());
+
+    let scheduler = setup_cron(config.clone(), data.clone());
+
+    if let Err(e) = scheduler.await.start().await {
+        warn!("failed to start scheduler {:?}", e);
+    }
+
+    info!("starting http server @ {}", address.clone());
 
     HttpServer::new(move || {
-        let token = Box::leak(konfig.secret.clone().into_boxed_str());
         App::new()
             .app_data(data.clone())
             .service(unlead95)
@@ -331,34 +293,10 @@ async fn main() -> std::io::Result<()> {
             .service(diesel_heat)
             .service(diesel_auto)
             .service(kerosene)
-            .service(
-                web::resource("/prices/1/refresh")
-                    .guard(guard::Header("X-TOKEN", token))
-                    .route(web::patch().to(refresh_unlead95)),
-            )
-            .service(
-                web::resource("/prices/2/refresh")
-                    .guard(guard::Header("X-TOKEN", token))
-                    .route(web::patch().to(refresh_unlead98)),
-            )
-            .service(
-                web::resource("/prices/3/refresh")
-                    .guard(guard::Header("X-TOKEN", token))
-                    .route(web::patch().to(refresh_diesel_heat)),
-            )
-            .service(
-                web::resource("/prices/4/refresh")
-                    .guard(guard::Header("X-TOKEN", token))
-                    .route(web::patch().to(refresh_diesel_auto)),
-            )
-            .service(
-                web::resource("/prices/5/refresh")
-                    .guard(guard::Header("X-TOKEN", token))
-                    .route(web::patch().to(refresh_kerosene)),
-            )
             .service(version)
     })
-    .bind(address)?
-    .run()
-    .await
+        .bind(address)
+        .unwrap()
+        .run()
+        .await.expect("server failed to start")
 }
